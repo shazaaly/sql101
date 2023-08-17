0c01992a8c5dbbade8c617acee031146ec2099e5
shazaaly <shaza.aly@gmail.com>
2023-08-17 04:49:37 +0300

    primary key

---
+++ more_queries/7-cities.sql
@@ -0,0 +1,6 @@
+-- a script that creates the database hbtn_0d_usa and the table cities
+
+-- cities description:
+-- id INT unique, auto generated, can’t be null and is a primary key
+-- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
+-- name VARCHAR(256) can’t be null
